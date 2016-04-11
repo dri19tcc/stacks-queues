c71d4d06823529a84217da7d2cfb7116abaae612
Adriana <dri19tcc@gmail.com>
2016-04-10 18:17:41 -0700

    First solved using a new class and methods of stacks and queues in one file.  Then used Stack and Queues that we made in class to make it work

--- Stack.rb
+++ Stack.rb
@@ -1,14 +1,23 @@
+# (LIFO)
 class Stack
+  attr_reader :store
   def initialize
     @store = Array.new
   end
-  
+
   def pop
+    @store.pop
   end
-  
+
   def push(element)
+    @store << element
   end
-  
+
   def size
+    @store.length
+  end
+
+  def empty?
+    size == 0
   end
 end
--- job-simulation.rb
+++ job-simulation.rb
@@ -1,4 +1,76 @@
 require './Stack.rb'
 require './Queue.rb'
 
+class Company
+  COMPANY_SIZE = 6
+  WAITING = 10
+  attr_reader :hired, :unemployed
 
+  def initialize
+    @hired = Stack.new #used as a stack
+    @unemployed = Queue.new # queue of peeps waiting for a job
+    WAITING.times do |n|
+      @unemployed.enqueue(n)
+    end
+  end
+
+  def hire_up_to(number = 6)
+    until @hired.size == number
+      person = @unemployed.dequeue
+      @hired.push(person)
+    end
+  end
+
+  def die
+    rand(1..6)
+  end
+
+  def downsize
+    to_fire = die
+    to_fire.times do
+      fire
+    end
+  end
+
+  def fire
+    @unemployed.enqueue(@hired.pop)
+  end
+
+  def employees
+    @hired.store
+  end
+end
+
+x = Company.new
+x.hire_up_to(Company::COMPANY_SIZE)
+x.downsize
+x.hire_up_to(Company::COMPANY_SIZE)
+puts x.employees #this is to print out all of my @store
+
+
+# class Company
+#   attr_reader :hire_line, :job_list
+#   def initialize
+#     @hire_line = (7..10).to_a #people waiting to be hired (FIFO)
+#     @job_list = (1..6).to_a #current employees to be fired (LIFO)
+#   end
+#
+#   def die
+#     rand(1..6)
+#   end
+#
+#   def hire
+#     until @job_list.length == 6
+#       @job_list << @hire_line.shift
+#     end
+#     return @job_list
+#   end
+#
+#   def fire
+#     to_fire = die
+#     to_fire.times do
+#       @hire_line << @job_list.pop
+#     end
+#     return @hire_line
+#   end
+# end
